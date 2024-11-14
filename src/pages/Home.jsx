e239f167ba41115b32aef6f9ef5b1b0539907ebc
ronitphilip <ronitpc1819@gmail.com>
2024-11-14 10:59:51 +0530

    fourth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@
 import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchProducts } from '../redux/slices/productSlice'
 
 const Home = () => {
   const dispatch = useDispatch()
-
+  const {allProducts,loading,errorMsg} = useSelector(state=>state.productReducer)
+  console.log(allProducts,loading,errorMsg);
+  
   useEffect(()=>{
     dispatch(fetchProducts())
   },[])
@@ -16,15 +18,34 @@ const Home = () => {
     <>
       <Header insideHome={true}/>
       <div style={{paddingTop:'100px'}} className="container px-4 mx-auto">
-        <div className="grid grid-cols-4 gap-4">
-          <div className="rounded border p-2 shadow">
-            <img width={'100%'} height={'200px'} src="https://picsum.photos/200/300" alt="image"/>
-            <div className="text-center">
-              <h3 className="text-xl font-bold">title</h3>
-              <Link to={'/id/view'} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block'>View More...</Link>
-            </div>
+        {
+          loading?
+          <div className="flex justify-center items-center my-5 text-lg">
+            <img width={'100px'} height={'100px'} src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="loading" />
+            Loading...
           </div>
-        </div>
+          :
+          <>
+            <div className="grid grid-cols-4 gap-4">
+              {
+                allProducts?.length>0 ?
+                allProducts?.map(product=>(
+                  <div key={product?.id} className="rounded border p-2 shadow">
+                    <img width={'100%'} height={'200px'} src={product?.thumbnail} alt="image"/>
+                    <div className="text-center">
+                      <h3 className="text-xl font-bold">{product.title}</h3>
+                      <Link to={`/${product?.id}/view`} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block'>View More...</Link>
+                    </div>
+                  </div>
+                ))
+                :
+                <div className="flex justify-center items-center font-bold text-red-600 my-5 text-lg">
+                  Product Not Found!!
+                </div>
+              }
+            </div>
+          </>
+        }
       </div>
     </>
   )
--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -4,16 +4,36 @@ import axios from "axios";
 //action return promise
 export const fetchProducts = createAsyncThunk("products/fetchProducts",async ()=>{
     const result = await axios.get("https://dummyjson.com/products")
-    console.log(result.data.products);
+    // console.log(result.data.products);
     return result.data.products
 })
 
 const productSlice = createSlice({
     name:'products',
     initialState:{
-        allProducts:[]
+        allProducts:[],
+        loading:false,
+        errorMsg:""
     },
     reducers:{
-
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchProducts.fulfilled,(state,apiResult)=>{
+            state.allProducts = apiResult.payload
+            state.loading = false
+            state.errorMsg = ""
+        })
+        builder.addCase(fetchProducts.pending,(state)=>{
+            state.allProducts = []
+            state.loading = true
+            state.errorMsg = ""
+        })
+        builder.addCase(fetchProducts.rejected,(state)=>{
+            state.allProducts = []
+            state.loading = false
+            state.errorMsg = "API Call Failed"
+        })
     }
 })
+
+export default productSlice.reducer;
